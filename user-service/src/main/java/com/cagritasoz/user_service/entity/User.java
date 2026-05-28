d049ee7e5c8ad989820f39ac0584b78f9886270d
Cagritasoz <cagritasoz@gmail.com>
2026-05-28 23:50:01 +0300

    add user entity, repository and persist logic
    - Introduced a JPA User entity with fields (id, firstName, lastName, email, address, alertsEnabled, energyAlertingThreshold) which correspond to flyway migration V1
    
    - Created UserRepository extending JpaRepository for easy persistance logic
    
    -Update UserService to use constructor-injected UserRepository (via Lombok @RequiredArgsConstructor) to persist incoming UserDto to the database and convert entities back to UserDto

---
+++ user-service/src/main/java/com/cagritasoz/user_service/entity/User.java
@@ -0,0 +1,36 @@
+package com.cagritasoz.user_service.entity;
+
+import jakarta.persistence.*;
+import lombok.*;
+
+@Entity
+@Table(name = "users")
+@Getter
+@Setter
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+public class User {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY) // BIGSERIAL
+    private Long id;
+
+    @Column(name = "first_name", length = 100, nullable = false)
+    private String firstName;
+
+    @Column(name = "last_name", length = 100, nullable = false)
+    private String lastName;
+
+    @Column(unique = true, nullable = false)
+    private String email;
+
+    @Column(columnDefinition = "TEXT")
+    private String address;
+
+    @Column(name = "alerts_enabled", nullable = false)
+    private Boolean alertsEnabled = false; // Defaults to false.
+
+    @Column(name = "energy_alerting_threshold", nullable = false)
+    private Double energyAlertingThreshold = 0.0; // Defaults to 0.0.
+}
--- user-service/src/main/java/com/cagritasoz/user_service/service/UserService.java
+++ user-service/src/main/java/com/cagritasoz/user_service/service/UserService.java
@@ -1,18 +1,47 @@
 package com.cagritasoz.user_service.service;
 
 import com.cagritasoz.user_service.dto.UserDto;
+import com.cagritasoz.user_service.entity.User;
+import com.cagritasoz.user_service.repository.UserRepository;
+import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class UserService {
 
+    private final UserRepository userRepository;
 
-    public UserDto createUser(UserDto userDto) {
-        // TODO: simulate user creation
-        log.info("Creating user {}", userDto);
-        return userDto;
+
+    public UserDto createUser(UserDto inputUser) {
+        log.info("Creating user {}", inputUser);
+
+        final User user = User.builder()
+                .firstName(inputUser.getFirstName())
+                .lastName(inputUser.getLastName())
+                .email(inputUser.getEmail())
+                .address(inputUser.getAddress())
+                .alertsEnabled(inputUser.isAlertsEnabled())
+                .energyAlertingThreshold(inputUser.getEnergyAlertingThreshold())
+                .build();
+
+        final User saved = userRepository.save(user); // id populated.
+
+        return toDto(user);
+    }
+
+    private UserDto toDto(User user) {
+        return UserDto.builder()
+                .id(user.getId())
+                .firstName(user.getFirstName())
+                .lastName(user.getLastName())
+                .email(user.getEmail())
+                .address(user.getAddress())
+                .alertsEnabled(user.getAlertsEnabled())
+                .energyAlertingThreshold(user.getEnergyAlertingThreshold())
+                .build();
     }
 }
 
