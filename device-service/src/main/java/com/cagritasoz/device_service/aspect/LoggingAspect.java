51a64809a4090858cd0cddcb5e7ebb983528cc33
Cagritasoz <cagritasoz@gmail.com>
2026-09-03 20:11:38 +0300

    add AOP logging and ingestion service scaffold
    - Device service now includes AOP-based request timing and service logging with controller execution metrics and exception tracking to match user-service module.
    
    - Introduced a new ingestion-service  as an empty spring boot module.

--- device-service/src/main/java/com/cagritasoz/device_service/aspect/LoggingAspect.java
+++ device-service/src/main/java/com/cagritasoz/device_service/aspect/LoggingAspect.java
@@ -1,4 +1,4 @@
-package com.cagritasoz.user_service.aspect;
+package com.cagritasoz.device_service.aspect;
 
 import lombok.extern.slf4j.Slf4j;
 import org.aspectj.lang.JoinPoint;
@@ -7,15 +7,15 @@ import org.springframework.stereotype.Component;
 
 import java.util.Arrays;
 
-
 @Aspect
-@Component // Very important to declare it as a bean.
+@Component
 @Slf4j
 public class LoggingAspect {
 
-    // All methods under service package.
-    @Pointcut("execution(* com.cagritasoz.user_service.service..*.*(..))")
-    public void serviceMethods() {}
+    @Pointcut("execution(* com.cagritasoz.device_service.service..*.*(..))")
+    public void serviceMethods() {
+
+    }
 
     @Before("serviceMethods()")
     public void logBefore(JoinPoint joinPoint) {
@@ -23,19 +23,16 @@ public class LoggingAspect {
                 Thread.currentThread().getName(),
                 joinPoint.getSignature().toShortString(),
                 Arrays.toString(joinPoint.getArgs()));
-
     }
 
-    // Unlike @After which acts like a finally block @AfterReturning runs only after successful method execution
-    @AfterReturning(pointcut = "serviceMethods()", returning = "result") //
-    public void logAfterReturning(JoinPoint joinPoint, Object result) { // Object "result" should match returning = "result"
+    @AfterReturning(pointcut = "serviceMethods()", returning = "result")
+    public void logAfterReturning(JoinPoint joinPoint, Object result) {
         log.info("Thread: [{}] called service method: [{}] returned: [{}]",
                 Thread.currentThread().getName(),
                 joinPoint.getSignature().toShortString(),
                 result);
     }
 
-    // Does not intercept/swallow the exception - it just observes it as it propagates, so no rethrow needed. It rethrows it internally!
     @AfterThrowing(pointcut = "serviceMethods()", throwing = "ex")
     public void logAfterThrowing(JoinPoint joinPoint, Throwable ex) {
         log.error("Thread: [{}] called service method: [{}] threw exception: [{}]",
